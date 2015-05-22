5faac9c30a4cddaabd8f274aaeff88c23c17493a
Trini Morrison <TRPMorrison@gmail.com>
2015-05-22 13:45:12 -0400

    COMMENTED OUT JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
-$('.title').text() // grabs the text inside of the element
-$('.container').html() // grabs the HTML inside of the element
+// $('.title').text() // grabs the text inside of the element
+// $('.container').html() // grabs the HTML inside of the element
 
-// Replace
-$('.title').text('New Title') // sets the text inside of the element
-$('.container').html('<a href="http://google.com">Google</a>') // sets the HTML inside of the element
+// // Replace
+// $('.title').text('New Title') // sets the text inside of the element
+// $('.container').html('<a href="http://google.com">Google</a>') // sets the HTML inside of the element
 
-// Append
-$('.container').append('<a href="http://google.com">Google</a>');
+// // Append
+// $('.container').append('<a href="http://google.com">Google</a>');
 
-// Prepend
-$('.container').prepend('<a href="http://google.com">Google</a>');
+// // Prepend
+// $('.container').prepend('<a href="http://google.com">Google</a>');
